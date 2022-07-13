d9c9a3fe95498f5350228481cfd76c9f8c633afb
v.nikul <nikul.victor@gmail.com>
2022-07-13 09:36:32 +0400

    PT-3 fixed refresh token and added every parameter to be optional

--- src/Service/UserService.php
+++ src/Service/UserService.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Service;
 
+use App\Entity\User;
 use App\Model\Request\EditUserRequest;
 use App\Model\Response\UserResponse;
 use App\Repository\UserRepository;
@@ -17,19 +18,24 @@ class UserService
 		private readonly UserRepository $repository,
 		private readonly EntityManagerInterface $entityManager,
 		private readonly Security $security,
+		private readonly UserPasswordHasherInterface $hasher
 	) {
 	}
 
 
 	public function editUser(EditUserRequest $request): UserResponse
 	{
-		$user = $this->repository->find($this->security->getUser()?->getUserIdentifier());
+		$user = $this->repository->find($this->security->getUser()?->getId());
 
 		if ($request->getEmail() !== null) {
 			$user->setEmail($request->getEmail());
 		}
 		if ($request->getAccountID() !== null) {
 			$user->setAccountId($request->getAccountId());
+		}
+		if ($request->getPassword() !== null) {
+			$hashed = $this->hasher->hashPassword($user, $request->getPassword());
+			$user->setPassword($hashed);
 		}
 		if ($request->getPhone()){
 			$user->setPhone($request->getPhone());
