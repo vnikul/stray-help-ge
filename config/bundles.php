cbc44a25fcade44b617c8e8d5ff743554a25d805
v.nikul <nikul.victor@gmail.com>
2022-08-16 11:03:01 +0400

    PH-7 added cors package

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
